416ba14648f92529288328a3fb6bf4afac68d5b0
DimasAditya <masdit966@gmail.com>
2021-09-22 08:50:02 +0700

    Menyelesaikan video pertemuan 2

---
+++ Pertemuan 2/index.php
@@ -0,0 +1,13 @@
+<?php
+/*
+Dimas Aditya
+193040128
+ https://github.com/dmsadtyaa/pw2021_193040128.git
+ Pertemuan 2 - 17 September 2021
+ Mempelajari mengenai sintaks php
+ */
+?>
+<?php
+// Standard Output
+// Echo, print
+?>
