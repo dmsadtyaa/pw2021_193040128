d4b6533089a8d2cacb8f1e120831bd5327edba86
DimasAditya <masdit966@gmail.com>
2021-09-23 12:58:57 +0700

    Menyelesaikan Video ke-4 tentang Constructor

---
+++ Pertemuan 2/construktor.php
@@ -0,0 +1,44 @@
+<?php
+/*
+Dimas Aditya
+193040128
+ https://github.com/dmsadtyaa/pw2021_193040128.git
+ Pertemuan 2 - 17 September 2021
+ Mempelajari mengenai sintaks php
+ */
+?>
+<?php
+// Jualan Produk
+// Komik
+// Game
+class Produk {
+    public $judul,
+           $penulis,
+           $penerbit,
+           $harga;
+
+    public function __construct($judul, $penulis, $penerbit, $harga) {
+        $this->judul = $judul;
+        $this->penulis = $penulis;
+        $this->penerbit = $penerbit;
+        $this->harga = $harga;
+
+    }
+
+   public function getLabel() {
+       return "$this->penulis, $this->penerbit";
+   }
+
+}
+
+$produk1 = new Produk("Naruto", "Masashi Kishimoto", "Shonen Jump", 30000);
+$produk2 = new Produk("Uncharted", "Neil Druckmann", "Sony Computer", 250000)
+$produk3 = new Produk("Dragon Ball");
+
+
+echo "Komik : " . $produk1->getLabel();
+echo "<br>";
+echo "Game : " . $produk2->getLabel();
+echo "<br>";
+var_dump($produk3); 
+?>
