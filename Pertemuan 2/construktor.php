3f3457ddd1772d122738ab829449b5596d40971e
DimasAditya <masdit966@gmail.com>
2021-09-23 13:19:10 +0700

    Menyelesaikan video tentang Constructor

--- Pertemuan 2/construktor.php
+++ Pertemuan 2/construktor.php
@@ -1,13 +1,4 @@
 <?php
-/*
-Dimas Aditya
-193040128
- https://github.com/dmsadtyaa/pw2021_193040128.git
- Pertemuan 2 - 17 September 2021
- Mempelajari mengenai sintaks php
- */
-?>
-<?php
 // Jualan Produk
 // Komik
 // Game
