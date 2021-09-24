9386d108b6b7f3379120bb480018eb9621cb9203
DimasAditya <masdit966@gmail.com>
2021-09-24 15:54:04 +0700

    Menyelesaikab Video tentang Overriding

--- Pertemuan 3/overriding.php
+++ Pertemuan 3/overriding.php
@@ -5,18 +5,16 @@ class produk {
            $penulis,
            $penerbit,
            $harga,
-           $jmlhalaman,
-           $waktuMain;
+          
+       
          
 
-           public function __construct( $judul = "judul", $penulis = "penulis", $penerbit = "penerbit", $harga = 0, $jmlHalaman = 0, $waktuMain = 0,) {
+           public function __construct( $judul = "judul", $penulis = "penulis", $penerbit = "penerbit", $harga = 0) {
                $this->judul = $judul;
                $this->penulis = $penulis;
                $this->penerbit = $penerbit;
                $this->harga = $harga;
-               $this->jmlHalaman = $jmlhalaman;
-               $this->waktuMain = $waktuMain;
-            
+               
            }
 
            public function getLabel(){
@@ -32,17 +30,34 @@ class produk {
             }
 }
            
-class Komik extends Produk{
+class Komik extends Produk
+    public $jmlHalaman;
+$judul = "judul", $penulis = "penulis", $penerbit = 
+    public function __construct("penerbit", $harga = 0, $jmlHalaman = 0) {
+    
+    parent: : __construct($judul, $penulis, $penerbit, $harga = 0, $jmlHalaman = 0) ;
+
+    $this->jmlHalaman = $jmlHalaman;
+
+    }
+
     public function getInfoProduk(){
-        $str = "Komik : {$this->judul} / {$this->getLabel()} (Rp. {$this->harga}) -{$this->jmlHalaman} Halaman.";
+        $str = "Komik : " . parent : : getInfoProduk() . " - {$this->jmlHalaman} Halaman.";
         return $str;
     } 
 
 }
+ 
+class Game extends Produk { 
+    public $waktuMain
 
-class Game extends Produk {
+
+    public function __construct($judul = "judul", $penulis = "penulis", $penerbit = "penerbit", $harga = 0, $waktuMain = 0) }
+    parent: :__construct($judul, $penulis, $penerbit, $harga = 0, $waktuMain = 0);
+    
+    $this->waktuMain $waktuMain;
     public function getInfoProduk() {
-    $str = "Game : {$this->judul} / {$this->getLabel()} (Rp. {$this->harga}) -{$this->waktuMain} Jam.";
+    $str = "Game : " . parent : : getInfoProduk() . " - {$this->waktuMain} Jam.";
     return $str;
     }
 
